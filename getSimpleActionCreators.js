43e083358a3d6e619b9c682076ca947df3699097
Anshul Sahni <anshul_sahni@live.com>
2017-02-07 00:37:45 +0530

    lint fixes

--- getSimpleActionCreators.js
+++ getSimpleActionCreators.js
@@ -1,22 +1,25 @@
-const capitalize = function(string) {
+const capitalize = function (string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-const getCamelCase = function(string) {
-  return string.split('_').map(function(word, index) {
+const getCamelCase = function (string) {
+  return string.split('_').map(function (word, index) {
     return index > 0 ? capitalize(word.toLowerCase()) : word.toLowerCase();
   }).join('');
 };
 
-module.exports = function() {
+const getAction = function (constant) {
+  return function () {
+    return {
+      type: constant
+    };
+  };
+};
+
+module.exports = function () {
   const methods = {};
-  for(constant in arguments) {
-    const _arguments = arguments;
-    methods[getCamelCase(arguments[constant])] = function() {
-      return {
-        type: _arguments[constant]
-      };
-    }
+  for (var constant = 0; constant < arguments.length; constant++) {
+    methods[getCamelCase(arguments[constant])] = getAction(arguments[constant]);
   }
   return methods;
 };
